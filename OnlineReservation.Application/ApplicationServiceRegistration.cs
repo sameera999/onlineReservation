b258583b6b78d5fb3560c91278c0d1b701420b78
Sameera <srasanga999@gmail.com>
2024-01-11 23:13:45 +0000

    Added automapper and MediatR

---
+++ OnlineReservation.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace OnlineReservation.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+
+            return services;
+        }
+    }
+}
---
+++ OnlineReservation.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineReservation.Domain.Contracts.Persistence
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IList<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+
+    }
+}
